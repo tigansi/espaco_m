833827f5fbc0fbc2c6e7d6b4d6e9d081840185f1
tigansi <tiagoigansi17@gmail.com>
2020-12-12 09:43:52 -0300

    Iniciado o painel de clientes

--- src/views/clientes/PainelClientes.vue
+++ src/views/clientes/PainelClientes.vue
@@ -1,3 +1,11 @@
 <template>
-  <ion-app> </ion-app>
+  <ion-app>
+    <ion-content id="page"> </ion-content>
+  </ion-app>
 </template>
+
+<style>
+#page {
+  --background: rgb(55, 52, 53);
+}
+</style>
